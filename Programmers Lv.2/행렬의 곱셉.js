ba3833c47bdab9bd78840ac515ca46b11df07881
computerkingminsu <ya5656@naver.com>
2024-03-05 20:35:22 +0900

    feat: algorithm

--- Programmers Lv.2/행렬의 곱셉.js
+++ Programmers Lv.2/행렬의 곱셉.js
@@ -1,17 +1,16 @@
 function solution(arr1, arr2) {
-  const newArr = [];
+  const result = [];
 
   for (let i = 0; i < arr1.length; i++) {
-    let result = [];
+    result[i] = [];
     for (let j = 0; j < arr2[0].length; j++) {
-      let elem = 0;
-      for (let k = 0; k < arr2.length; k++) {
-        // arr1[0].length도 가능.
-        elem += arr1[i][k] * arr2[k][j];
+      let sum = 0;
+      for (let k = 0; k < arr1[0].length; k++) {
+        sum += arr1[i][k] * arr2[k][j];
       }
-      result.push(elem);
+      result[i][j] = sum;
     }
-    newArr.push(result);
   }
-  return newArr;
+
+  return result;
 }
