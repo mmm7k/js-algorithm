8f3f2a7fca106bfa855dbae0c23a44df6c14b86f
computerkingminsu <ya5656@naver.com>
2024-04-04 11:36:26 +0900

    feat: algorithm

--- Programmers Lv.2/멀리 뛰기.js
+++ Programmers Lv.2/멀리 뛰기.js
@@ -1,9 +1,11 @@
 function solution(n) {
-  const dp = Array(n + 1).fill(0);
-  dp[1] = 1;
-  dp[2] = 2;
-  for (let i = 3; i <= n; i++) {
-    dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+  let answer = 0;
+  let dp=[];
+  dp[1]=1;
+  dp[2]=2;
+  for(var i=3;i<=n;i++){
+      dp[i]=dp[i-1]+dp[i-2] %1234567;
   }
-  return dp[n];
+  answer=dp[n];
+  return answer%1234567;
 }
