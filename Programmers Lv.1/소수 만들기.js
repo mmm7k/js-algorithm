1106f4272a7ed278dcdcbc7dd0b615d2220fd675
computerkingminsu <ya5656@naver.com>
2024-08-26 23:00:14 +0900

    feat: algorithm

---
+++ Programmers Lv.1/소수 만들기.js
@@ -0,0 +1,24 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) {
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+}
